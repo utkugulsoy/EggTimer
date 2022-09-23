b25f92e9f458bb9f753fb62075bf8f95e83b738a
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2022-09-23 12:40:38 +0300

    EggTimer project, created, finished.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,14 +6,78 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var player: AVAudioPlayer?
+    var counter = 0
+    var tim: Timer?
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        stopTimer()
+        
+        let hardness = sender.currentTitle
+        if hardness == "Soft"
+        {
+            counter = 300
+        }
+        else if hardness == "Medium"
+        {
+            counter = 420
+        }
+        else
+        {
+            counter = 720
+        }
+        startTimer(limit: counter)
+        titleLabel.text = hardness
+        progressBar.progress = 0
+        secondsPassed = 0
     }
+    
+    func startTimer(limit:Int)
+    {
+        guard tim == nil else {return}
+        tim = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    func stopTimer()
+    {
+        tim?.invalidate()
+        tim = nil
+    }
+    @objc func updateCounter()
+    {
+        secondsPassed += 1
+        let progress = Float(secondsPassed)/Float(counter)
+        progressBar.progress = progress
+        if progress == 1
+        {
+            stopTimer()
+            titleLabel.text = "DONE!"
+            playSound()
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")!
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
 
+            player.prepareToPlay()
+            player.play()
+
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
 }
 
